eaef36de744dc19bff82ced21fe48667ecf16b2c
Prasham1710 <jainprasham17@gmail.com>
2023-01-25 04:42:02 +0530

    type

--- src/componets/TodoForm.js
+++ src/componets/TodoForm.js
@@ -2,13 +2,16 @@ import React, { useState} from 'react'
 
 function TodoForm() {
     const [input, setInput ] = useState('');
+
+    
   return (
     <div>
         <input
-              
+              className="shadow appearance-none border rounded focus:shadow-outline"
               id="Height "
               type="text"
               value={input}
+              name= 'text'
               placeholder="Height in cm"
               />
         
