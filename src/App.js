ec0a31a2dadcd466810e436af3822f6309aca400
Prasham1710 <jainprasham17@gmail.com>
2023-04-04 20:24:20 +0530

    colour

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const deleteTodo  = async(id) => {
     await deleteDoc(doc(db, 'todos',id))
 }
   return (
-    <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'>
+    <div className='h-screen w-screen p-4 bg-gradient-to-r from-indigo-500 to-pink-500'>
         <div className='bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4'>
             <h3 className='text-3xl font-bold text-center text-gray-800 p-2'>Todo App</h3>
             <form onSubmit={createTodo} className='flex justify-between'>
